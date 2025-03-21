a8332c733f1b224395df63d3f3ce7ac9ad5700c5
Piskun Ivan <tw1st3r111@mail.ru>
2025-03-21 20:05:30 +0300

    fixed issues vite -build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: './',
   server: { port: 3000 },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'src/css/images/**/*', dest: 'src/css/images' },
+        { src: 'src/css/icons/**/*', dest: 'src/css/icons' },
+      ],
+    }),
+  ],
+  assetsInclude: ['**/*.svg'],
 });
